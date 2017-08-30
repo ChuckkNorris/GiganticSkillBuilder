223f3c1297921bfca2f2fe659c6b6eed007ec8e7
Levi Fuller <lfuller941@gmail.com>
2017-08-30 00:31:29 -0600

    Cleaned up portrait scaffolding;

--- GiganticSkillBuilder/src/components/portrait/portrait.js
+++ GiganticSkillBuilder/src/components/portrait/portrait.js
@@ -1,24 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './portrait.css';
-import { Grid, Row, Col } from 'react-flexbox-grid';
+import {Grid, Row, Col} from 'react-flexbox-grid';
 
 //
 export class Portrait extends Component {
   render() {
     return (
       <Grid fluid>
-      <Row>
-        <Col xs={6} sm={12}>
-          <div className="circle"></div>
-        </Col>
-        <Col xs={6} sm={12}>
-          <div className="circle"></div>
-        </Col>
-        <Col xs={6} sm={12}>
-          <div className="circle"></div>
-        </Col>
-      </Row>
-    </Grid>
+        <Row>
+
+            <div className="circle align-center"></div>
+
+        </Row>
+      </Grid>
     );
   }
 }
